1543cf0272a4df49aa24cde344a62a6a594b9aa8
Zenderg <danil54658798@gmail.com>
2020-02-20 14:40:18 +0300

    access CORS

--- server/db_service.py
+++ server/db_service.py
@@ -7,14 +7,9 @@ from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "localhost",
-    "localhost:4200",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
